fb70a3f99705fb15ee299ce0a7317d0ed75a0006
chris <chris@f8c72719-715d-4dca-a8c2-a74f8bfefce5>
2014-11-10 22:18:55 +0000

    Added ant build files for android application

---
+++ se.qxx.android.fiatlux/gen/R.java.d
@@ -0,0 +1,15 @@
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/gen/se/qxx/android/fiatlux/R.java \
+ : /home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/menu/fiat_lux_main.xml \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/drawable-mdpi/ic_launcher.png \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/values/strings.xml \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/values/dimens.xml \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/values/styles.xml \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/drawable-xxhdpi/ic_launcher.png \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/values-v14/styles.xml \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/values-v11/styles.xml \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/values-w820dp/dimens.xml \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/drawable-hdpi/ic_launcher.png \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/layout/item_fiat_lux_device.xml \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/layout/activity_fiat_lux_main.xml \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/res/drawable-xhdpi/ic_launcher.png \
+/home/ubuntu/workspace/fiatlux/trunk/se.qxx.android.fiatlux/bin/AndroidManifest.xml \
