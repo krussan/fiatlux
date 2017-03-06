e5604abc06298b9f3bb93f6589945a08bc34bf5e
Christian Thunström <christian@qxx.se>
2017-03-06 14:44:50 +0100

    cache the correct directory

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -6,10 +6,11 @@ pwd
 if [ ! -d protobuf-2.4.1 ]; then
    wget https://github.com/google/protobuf/releases/download/v2.4.1/protobuf-2.4.1.tar.gz
    tar -xzvf protobuf-2.4.1.tar.gz
-   cd ~/protobuf-2.4.1 && ./configure --prefix=/usr && make 
+   cd $TRAVIS_BUILD_DIR/protobuf-2.4.1 && ./configure --prefix=/usr && 
+make 
 fi
 
-cd ~/protobuf-2.4.1
+cd $TRAVIS_BUILD_DIR/protobuf-2.4.1
 sudo make install
 
 
