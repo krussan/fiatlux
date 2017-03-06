91005598394e045c2f3aceb70f4c07bf8c167ddc
Christian Thunström <christian@qxx.se>
2017-03-06 14:40:40 +0100

    Where am I

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+pwd
+
 if [ ! -d protobuf-2.4.1 ]; then
    wget https://github.com/google/protobuf/releases/download/v2.4.1/protobuf-2.4.1.tar.gz
    tar -xzvf protobuf-2.4.1.tar.gz
