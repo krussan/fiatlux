c65ec88febc3eea18d7e530259f28fe43305c267
Christian Thunström <christian@qxx.se>
2017-03-06 14:33:55 +0100

    Added protobuf download to cache

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -ex
-wget https://github.com/google/protobuf/releases/download/v2.4.1/protobuf-2.4.1.tar.gz
-tar -xzvf protobuf-2.4.1.tar.gz
-cd protobuf-2.4.1 && ./configure --prefix=/usr && make && sudo make install
+
+if [ ! -d protobuf-2.4.1 ]; then
+   wget https://github.com/google/protobuf/releases/download/v2.4.1/protobuf-2.4.1.tar.gz
+   tar -xzvf protobuf-2.4.1.tar.gz
+   cd protobuf-2.4.1 && ./configure --prefix=/usr && make 
+fi
+
+sudo make install
 
 
