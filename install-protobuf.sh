4e6ae9b304e714d5a60b8fc4046130779f5c8491
Christian Thunström <christian@qxx.se>
2017-03-06 14:36:41 +0100

    cd to the correct protobuf directory

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -4,9 +4,10 @@ set -ex
 if [ ! -d protobuf-2.4.1 ]; then
    wget https://github.com/google/protobuf/releases/download/v2.4.1/protobuf-2.4.1.tar.gz
    tar -xzvf protobuf-2.4.1.tar.gz
-   cd protobuf-2.4.1 && ./configure --prefix=/usr && make 
+   cd ~/protobuf-2.4.1 && ./configure --prefix=/usr && make 
 fi
 
+cd ~/protobuf-2.4.1
 sudo make install
 
 
