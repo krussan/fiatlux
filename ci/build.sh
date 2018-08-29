9845cbc4575a93aa834bb7fb4a7270520cfbd479
krussan <christian@qxx.se>
2018-08-29 20:27:06 +0200

    Fix main class (#8)
    * Added main class to gradle script
    
    * Updated icon
    
    * Added web hi res icon
    
    * Updating for release
    
    * Updated travis android sdk references
    
    * Handle github releases and pull request builds
    
    * Removed deploy test folder
    
    * Fixed library path
    
    * Added dummy gradle initialization
    
    * more travis configuration

---
+++ ci/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo Initiating build ...
+
+echo
+echo -----------------------------------------------------
+echo VERSION :: $FIATLUX_VERSION
+echo TRAVIS_PULL_REQUEST :: $TRAVIS_PULL_REQUEST
+echo TRAVIS_BRANCH :: $TRAVIS_BRANCH
+echo -----------------------------------------------------
+echo
+
+if [[ "$TRAVIS_BRANCH" == "master" ]];then
+   echo Checking that the resulting tag does not exist
+
+   if git rev-parse -q --verify "refs/tags/$FIATLUX_TAG" >/dev/null; then
+      echo ERROR! Tag $FIATLUX_VERSION exist. Please modify pom and commit.
+      exit 1
+   fi
+fi
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "master" ]];then
+   echo Packaging new release ...
+   ./gradlew clean build check connectedCheck assemble packageRelease archiveZip publishRelease -PprotocExec=${TRAVIS_BUILD_DIR}/ci/protoc
+else 
+   echo Running test ...
+   ./gradlew clean build check connectedCheck -PprotocExec=${TRAVIS_BUILD_DIR}/ci/protoc
+fi
+
