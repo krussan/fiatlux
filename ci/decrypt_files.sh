7d631b860fcb4f55fe860aacdc662be3fc6c1e21
Christian Thunström <christiant@spotify.com>
2023-08-21 11:30:41 +0200

    wip

--- ci/decrypt_files.sh
+++ ci/decrypt_files.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/keystore.enc -out ci/keystore
-openssl aes-256-cbc -d -k "$TRAVIS_IDENTITY" -in ci/travis.enc -out ~/.ssh/id_rsa
-openssl aes-256-cbc -d -k "$TRAVIS_IDENTITY" -in ci/api.enc -out ci/api.json
+openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/keystore2.enc -out ci/keystore2
+#openssl aes-256-cbc -d -k "$TRAVIS_IDENTITY" -in ci/travis.enc -out ~/.ssh/id_rsa
+#openssl aes-256-cbc -d -k "$TRAVIS_IDENTITY" -in ci/api.enc -out ci/api.json
 
 chmod 600 ~/.ssh/id_rsa
