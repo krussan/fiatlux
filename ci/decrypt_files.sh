980549b6c7c432f9f3b83485afaa43927fcb869a
Christian Thunström <christiant@spotify.com>
2023-08-21 11:34:49 +0200

    remove chmod

--- ci/decrypt_files.sh
+++ ci/decrypt_files.sh
@@ -4,4 +4,4 @@ openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/keystore2.enc -out ci/keystore
 #openssl aes-256-cbc -d -k "$TRAVIS_IDENTITY" -in ci/travis.enc -out ~/.ssh/id_rsa
 #openssl aes-256-cbc -d -k "$TRAVIS_IDENTITY" -in ci/api.enc -out ci/api.json
 
-chmod 600 ~/.ssh/id_rsa
+#chmod 600 ~/.ssh/id_rsa
